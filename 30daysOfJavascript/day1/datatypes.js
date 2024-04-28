309cb6d7db747fe76eb529b5c1fff7500940dbd3
Ahmed Hamza <134783176+Hamzacakin@users.noreply.github.com>
2024-04-28 20:42:23 +0300

    js added

---
+++ 30daysOfJavascript/day1/datatypes.js
@@ -0,0 +1,24 @@
+console.log(typeof hamza);
+console.log(typeof "31");
+console.log(typeof 31);
+console.log(typeof false);
+
+
+let name;
+let surName;
+let age;
+let isSpeakTurkish;
+let yok;
+
+let isMarried = false;
+const name2 = "ahmed hamza";
+const surName2 = "çakın";
+let age2 = 33;
+let isSpeakTurkish2 = true;
+let yok2;
+
+console.log(name2,surName2,age2,isMarried);
+
+let myAge = 33;
+let yourAge = 30;
+console.log("I`am "+ myAge + " years old. " , "You are " + yourAge + " years old");
